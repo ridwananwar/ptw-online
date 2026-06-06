9bfb9e4f52b8553abad00aa7b7fd86542d090222
jambimerang <jambimerangfolder@gmail.com>
2026-06-06 21:07:18 +0700

    Pembaruan: Menambahkan start script di package.json dan port dinamis

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
